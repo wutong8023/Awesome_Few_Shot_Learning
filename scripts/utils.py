2714646587eaaa0d8b6d2a03792cb177fb9fc6d7
wutong8023 <wutong8023@gmail.com>
2021-06-26 01:31:02 +1000

    update

--- scripts/utils.py
+++ scripts/utils.py
@@ -107,7 +107,7 @@ def get_md_entry(DB, entry, add_comments=True):
             md_str += '\n```'
     md_str += "</details>"
 
-    md_str += '<details><summary><img src="./scripts/svg/copy.png" height="20" vspace=50></summary>'
+    md_str += '<details><summary><img src="./scripts/svg/copy.png" height="20" vspace="-50"></summary>'
     md_str += f"<pre>```{entry['ID']}```"
 
     # md_str += '<br>\n'
