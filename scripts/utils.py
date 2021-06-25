b1e1d3bafaecfaae7cbfe740e32e68c50eef3a44
wutong8023 <wutong8023@gmail.com>
2021-06-26 03:16:34 +1000

    update

--- scripts/utils.py
+++ scripts/utils.py
@@ -111,8 +111,8 @@ def get_md_entry(DB, entry, add_comments=True):
             md_str += DB.strings[entry['ID'].lower()]
             md_str += '\n```'
     md_str += "</details>"
-    
-    img_link = base_link + "scripts/svg/copy_icon.png"
+
+    img_link = os.path.join(base_link, "scripts/svg/copy_icon.png")
     md_str += f'<details><summary><img src={img_link} height="20"></summary>'
     md_str += f"<pre>```{entry['ID']}```"
     
